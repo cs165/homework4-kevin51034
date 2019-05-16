9db07d55580e157525f1996639c7fe8cba3cdf99
kevin51034 <48235276+kevin51034@users.noreply.github.com>
2019-05-16 22:45:17 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,25 @@
 class App {
   constructor() {
     // TODO(you): Implement the constructor and add fields as necessary.
+    const menuElement = document.querySelector('#menu');
+    const musicElement = document.querySelector('#music');
+
+    this.toMusic = this.toMusic.bind(this);
+    document.addEventListener('toMusic', this.toMusic);
+
+    this.menu = new MenuScreen(menuElement);
+    this.music = new MusicScreen(musicElement);
+
   }
   // TODO(you): Add methods as necessary.
+
+  toMusic() {
+    document.querySelector('#menu').classList.add("inactive");
+    document.querySelector('#music').classList.remove("inactive");
+    //console.log(this.menu.songUrl)
+    //console.log(document.getElementById("query-input").value)
+
+    document.dispatchEvent(new CustomEvent('getGifs'));
+
+  }
 }
--- music-screen.js
+++ music-screen.js
@@ -8,8 +8,44 @@
 //
 // See HW4 writeup for more hints and details.
 class MusicScreen {
-  constructor() {
+  constructor(musicElement) {
     // TODO(you): Implement the constructor and add fields as necessary.
+    console.log(this);
+    console.log(this.songs);
+
+    this.getGifs = this.getGifs.bind(this);
+    document.addEventListener('getGifs', this.getGifs);
+
+    this.gifJsonReady = this.gifJsonReady.bind(this);
+
   }
   // TODO(you): Add methods as necessary.
+  getGifs() {
+    console.log('getGifs');
+    console.log(app.menu.songUrl);
+    console.log(document.getElementById("query-input").value)
+
+    const theme = document.getElementById("query-input").value;
+    //const textBox = document.querySelector("#query-input");
+    const endpoint = "https://api.giphy.com/v1/gifs/search?q=" + theme +
+     "&api_key=dc6zaTOxFJmzC&limit=25&rating=g";
+     console.log(endpoint)
+
+   fetch(endpoint)
+   //.then(response => response.json())
+   .then(function(response) {
+     return response.json();
+   })
+   .then(this.gifJsonReady);
+// dance will error
+
+  }
+  gifJsonReady(json) {
+    const data = json.data;
+    console.log(data)
+    console.log(json)
+    const gifUrl = json.data[0].images.downsized.url;
+    console.log(gifUrl)
+    document.getElementById('gif').style.backgroundImage = `url(${gifUrl})`;
+  }
 }
